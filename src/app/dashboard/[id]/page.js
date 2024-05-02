1a22adb30ef2cd5b6439279d78cef1387c3c10d3
Ahmdpolash <ahmedpolash732@gmail.com>
2024-05-02 13:09:07 +0600

    added task on ui

--- src/app/dashboard/[id]/page.js
+++ src/app/dashboard/[id]/page.js
@@ -5,10 +5,28 @@ import { FcHighPriority } from "react-icons/fc";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { FaEdit } from "react-icons/fa";
-import { Button, Modal, Table, Space, message,DatePicker, Form, Input } from "antd";
+import moment from "moment";
+
+import {
+  Button,
+  Modal,
+  Table,
+  Space,
+  message,
+  DatePicker,
+  Form,
+  Input,
+  Select,
+} from "antd";
+import axios from "axios";
 const Details = ({ params }) => {
   const [data, setData] = useState([]);
   const [modal2Open, setModal2Open] = useState(false);
+
+  const formattedStartDate = moment(data?.task?.start_date).format(
+    "YYYY-MM-DD"
+  );
+  const formattedDeadline = moment(data?.task?.deadline).format("YYYY-MM-DD");
 
   let url = "https://6630e1d3c92f351c03db7082.mockapi.io/api/v1/projects";
 
@@ -39,18 +57,31 @@ const Details = ({ params }) => {
 
   const handleSubmit = async (values) => {
     try {
-      const res = await axios.post(
-        "https://6630e1d3c92f351c03db7082.mockapi.io/api/v1/projects",
-        values
+      const newTask = {
+        task_name: values.task_name,
+        start_date: values.start_date,
+        deadline: values.deadline,
+        assignedMembers: values.assignedMembers,
+        description: values.description,
+      };
+
+      const updatedProject = {
+        ...data,
+        task: newTask,
+      };
+
+      const res = await axios.put(
+        `https://6630e1d3c92f351c03db7082.mockapi.io/api/v1/projects/${params.id}`,
+        updatedProject
       );
 
-      if (res.status === 201) {
-        message.success("Project added successfully");
-        router.push("/dashboard/projects");
-        form.resetFields();
+      if (res.status === 200) {
+        message.success("Task added successfully");
+        setModal2Open(false);
+        refetch();
       }
     } catch (error) {
-      console.error("Error adding project:", error);
+      console.error("Error adding task:", error);
     }
   };
 
@@ -78,7 +109,8 @@ const Details = ({ params }) => {
               <div className=" bg-white rounded-lg ">
                 <div className="p-4 space-y-1 shadow-md">
                   <div className="flex justify-between">
-                    <h1 className="font-bold">{data?.project_name}</h1>
+                    <h1 className="font-bold">{data?.task?.task_name}</h1>
+
                     <div className="flex gap-2">
                       <button>
                         <FaEdit className="text-[22px] text-[#5082e8]" />
@@ -86,11 +118,15 @@ const Details = ({ params }) => {
                       <MdDelete className="text-red-500 text-[25px]" />
                     </div>
                   </div>
+                  <div>
+                    <p>Start Date :{formattedStartDate}</p>
+                    <p>Deadline :{formattedDeadline}</p>
+                  </div>
                   <p className="flex font-semibold items-center gap-2">
                     {" "}
                     <FcHighPriority />
                   </p>
-                  <p className="text-gray-600">{data?.description}</p>
+                  <p className="text-gray-600">{data?.task?.description}</p>
                   <hr />
                   <p className="flex gap-2 items-center">
                     {" "}
@@ -136,29 +172,73 @@ const Details = ({ params }) => {
                 Task Name *
               </label>
               <Form.Item
-                name="project_name"
+                name="task_name"
                 rules={[
                   { required: true, message: "Please enter the project name" },
                 ]}
               >
-                <Input style={{ height: "50px" }} />
+                <Input style={{ height: "40px" }} />
               </Form.Item>
             </div>
 
-            <div className="flex w-full flex-col space-y-2 mb-2">
-              <label htmlFor="start_date" className="text-[16px]">
-                Start Date
+            <div className="flex flex-col lg:flex-row gap-3 mb-2 lg:mb-0 w-full">
+              <div className="flex w-full flex-col space-y-2 mb-2">
+                <label htmlFor="start_date" className="text-[16px]">
+                  Start Date
+                </label>
+                <Form.Item
+                  name="start_date"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please select the project start date",
+                    },
+                  ]}
+                >
+                  <DatePicker style={{ height: "40px" }} className="w-full" />
+                </Form.Item>
+              </div>
+
+              <div className="flex w-full flex-col space-y-2 mb-2 ">
+                <label htmlFor="deadline" className="text-[16px]">
+                  Deadline
+                </label>
+                <Form.Item
+                  name="deadline"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please select the project deadline",
+                    },
+                  ]}
+                >
+                  <DatePicker style={{ height: "40px" }} className="w-full" />
+                </Form.Item>
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-2 mb-2">
+              <label htmlFor="assignedMembers" className="text-[16px]">
+                Assigned Members
               </label>
               <Form.Item
-                name="start_date"
+                name="assignedMembers"
                 rules={[
                   {
                     required: true,
-                    message: "Please select the project start date",
+                    message: "Please select the assigned members",
                   },
                 ]}
               >
-                <DatePicker className="w-full" />
+                <Select
+                  style={{ height: "40px" }}
+                  mode="multiple"
+                  placeholder="Select members"
+                >
+                  <Option value="member1">Member 1</Option>
+                  <Option value="member2">Member 2</Option>
+                  <Option value="member3">Member 3</Option>
+                </Select>
               </Form.Item>
             </div>
 
@@ -175,7 +255,7 @@ const Details = ({ params }) => {
                   },
                 ]}
               >
-                <Input.TextArea style={{ height: "180px" }} />
+                <Input.TextArea style={{ height: "150px" }} />
               </Form.Item>
             </div>
 
@@ -185,7 +265,7 @@ const Details = ({ params }) => {
                 type="primary"
                 htmlType="submit"
               >
-                Update Project
+                Add Task
               </Button>
             </Form.Item>
           </Form>
